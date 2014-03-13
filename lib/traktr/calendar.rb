60e30d04abffd61907b17ec7d2f586ab9548d333
Joe Lanford <joe.lanford@gmail.com>
2014-03-13 02:37:33 -0400

    minor updates to calendar endpoints

--- lib/traktr/calendar.rb
+++ lib/traktr/calendar.rb
@@ -11,9 +11,10 @@ module Traktr
     ##
     ## calendar GET methods
     ##
-    def premieres(date = Date.today.strftime("%Y%m%d"), days = 7)
-      date = date.class == Date ? date.strftime("%Y%m%d") : date
-      response = self.class.get('/' + File.join('premieres.json', @client.api_key, date.to_s, days.to_s), :basic_auth => @auth)
+    def premieres(date = Date.today, days = 7)
+      date = date.class == Date ? date.strftime("%Y%m%d") : date.to_s
+
+      response = self.class.get('/' + File.join('premieres.json', @client.api_key, date, days.to_s), :basic_auth => @auth)
       raise ResponseError.new(response) if response.code != 200
 
       response.parsed_response.collect do |data|
@@ -21,9 +22,10 @@ module Traktr
       end
     end
 
-    def shows(date = Date.today.strftime("%Y%m%d"), days = 7)
-      date = date.class == Date ? date.strftime("%Y%m%d") : date
-      response = self.class.get('/' + File.join('shows.json', @client.api_key, date.to_s, days.to_s), :basic_auth => @auth)
+    def shows(date = Date.today, days = 7)
+      date = date.class == Date ? date.strftime("%Y%m%d") : date.to_s
+
+      response = self.class.get('/' + File.join('shows.json', @client.api_key, date, days.to_s), :basic_auth => @auth)
       raise ResponseError.new(response) if response.code != 200
 
       response.parsed_response.collect do |data|
