91c3b3cc8b8cf2515b90e32f7a532074b1fe2e85
Joe Lanford <joe.lanford@gmail.com>
2014-03-13 21:56:28 -0400

    minor updates to movies endpoints

--- lib/traktr/movies.rb
+++ lib/traktr/movies.rb
@@ -14,13 +14,15 @@ module Traktr
       response = self.class.get('/' + File.join('trending.json', @client.api_key))
       raise ResponseError.new(response) if response.code != 200
 
-      response.parsed_response.collect do |movie|
-        Mash.new(movie)
+      response.parsed_response.collect do |data|
+        Mash.new(data)
       end
     end
 
     def updated(timestamp)
-      response = self.class.get('/' + File.join('updated.json', @client.api_key, timestamp.to_i.to_s))
+      timestamp = timestamp.class == Time ? timestamp.to_i.to_s : timestamp.to_s
+
+      response = self.class.get('/' + File.join('updated.json', @client.api_key, timestamp))
       raise ResponseError.new(response) if response.code != 200
 
       Mash.new(response.parsed_response)
