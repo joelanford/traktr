9712a6eba904030abeecb716c3b2e490a7164039
Joe Lanford <joe.lanford@gmail.com>
2014-03-13 22:20:05 -0400

    minor updates to search endpoints

--- lib/traktr/search.rb
+++ lib/traktr/search.rb
@@ -5,53 +5,56 @@ module Traktr
 
     def initialize(client)
       @client = client
+      @auth = { :username => @client.username, :password => @client.password }
     end
     
     ##
     ## search GET methods
     ##
-    def episodes(query)
-      response = self.class.get('/' + File.join('episodes.json', @client.api_key, URI::encode(query)))
+    def episodes(query, limit = 30)
+      response = self.class.get('/' + File.join('episodes.json', @client.api_key, URI::encode(query), limit.to_s))
       raise ResponseError.new(response) if response.code != 200
 
-      response.parsed_response.collect do |result|
-        Mash.new(result)
+      response.parsed_response.collect do |data|
+        Mash.new(data)
       end
     end
 
-    def movies(query)
-      response = self.class.get('/' + File.join('movies.json', @client.api_key, URI::encode(query)))
+    def movies(query, limit = 30)
+      response = self.class.get('/' + File.join('movies.json', @client.api_key, URI::encode(query), limit.to_s))
       raise ResponseError.new(response) if response.code != 200
 
-      response.parsed_response.collect do |result|
-        Mash.new(result)
+      response.parsed_response.collect do |data|
+        Mash.new(data)
       end
     end
 
-    def people(query)
-      response = self.class.get('/' + File.join('people.json', @client.api_key, URI::encode(query)))
+    def people(query, limit = 30)
+      response = self.class.get('/' + File.join('people.json', @client.api_key, URI::encode(query), limit.to_s))
       raise ResponseError.new(response) if response.code != 200
 
-      response.parsed_response.collect do |result|
-        Mash.new(result)
+      response.parsed_response.collect do |data|
+        Mash.new(data)
       end
     end
 
     def shows(query, limit = 30, seasons = nil)
-      response = self.class.get('/' + File.join('shows.json', @client.api_key, URI::encode(query), limit.to_s, seasons.to_s))
+      seasons = seasons == nil || seasons == false ? "" : "seasons"
+
+      response = self.class.get('/' + File.join('shows.json', @client.api_key, URI::encode(query), limit.to_s, seasons))
       raise ResponseError.new(response) if response.code != 200
 
-      response.parsed_response.collect do |result|
-        Mash.new(result)
+      response.parsed_response.collect do |data|
+        Mash.new(data)
       end
     end
 
-    def users(query)
-      response = self.class.get('/' + File.join('users.json', @client.api_key, URI::encode(query)))
+    def users(query, limit = 30)
+      response = self.class.get('/' + File.join('users.json', @client.api_key, URI::encode(query), limit.to_s), :basic_auth => @auth)
       raise ResponseError.new(response) if response.code != 200
 
-      response.parsed_response.collect do |result|
-        Mash.new(result)
+      response.parsed_response.collect do |data|
+        Mash.new(data)
       end
     end
 
