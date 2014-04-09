4a2c937714c7819cdb81dcd2ad2e9ee93f638429
Joe Lanford <joe.lanford@gmail.com>
2014-04-10 00:22:36 +0100

    fixed account/create endpoint

--- lib/traktr/account.rb
+++ lib/traktr/account.rb
@@ -2,7 +2,7 @@ module Traktr
   class Account < Endpoint
     def create(username, password, email, sha1 = false)
       data = { :username => username, :password => sha1 ? password : Digest::SHA1.hexdigest(password), :email => email }
-      response = self.class.post("/" + File.join("create", @client.api_key), body: @auth.to_json, headers: { 'Content-Type' => 'application/json'})
+      response = self.class.post("/" + File.join("create", @client.api_key), body: data.to_json, headers: { 'Content-Type' => 'application/json'})
       parse_response(response)
     end
 
